737c5c0776cad21e793c306628e1cb7e774c5ca2
Luke074 <mendeslucas277@gmail.com>
2021-02-18 15:56:52 -0300

    terminando a parte de consoles

--- app/src/main/java/com/example/testefragment/data/dao/ConsoleDataSource.kt
+++ app/src/main/java/com/example/testefragment/data/dao/ConsoleDataSource.kt
@@ -24,6 +24,31 @@ class ConsoleDataSource {
                 "PlayStation",
                 "1994-12-03",
                 context.getDrawable(R.drawable.play)!!))
+            consoles.add(Console(4,
+                "PlayStation 2",
+                "PlayStation",
+                "2000-03-04",
+                context.getDrawable(R.drawable.play2)!!))
+            consoles.add(Console(5,
+                "Playstation 3",
+                "PlayStation",
+                "2006-11-11",
+                context.getDrawable(R.drawable.play3)!!))
+            consoles.add(Console(6,
+                "Playstation 4",
+                "PlayStation",
+                "2013-11-15",
+                context.getDrawable(R.drawable.ps4)!!))
+            consoles.add(Console(7,
+                "Nintendo Switch",
+                "Nintendo",
+                "2017-03-03",
+                context.getDrawable(R.drawable.nintendo_switch)!!))
+            consoles.add(Console(8,
+                "Nintendo 64",
+                "Nintendo",
+                "1996-06-23",
+                context.getDrawable(R.drawable.nintendo_64)!!))
 
 
             return consoles
